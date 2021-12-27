40dbd6feabf2c67919f3ebbe9cb37064884ff87f
alfredo <alfredo500-@hotmail.com>
2021-12-27 17:30:35 -0600

    guardando spring boot

---
+++ src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,18 @@
+package com.in28minutes.springboot.basics.springbootin10steps;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+	@GetMapping("/books")
+	public List<Book> getAllBooks()
+	{
+		return Arrays.asList(new Book(11,"Apende a programar 1.1.0","Alfredo"));
+	}
+	
+}
